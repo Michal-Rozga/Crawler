3b77b1c1d21a6e1da87a577b4a7344b485dee800
Michal <michal.rozga@yahoo.com>
2024-10-21 15:56:57 +0200

    Created one object to pass it to both functions

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -4,83 +4,58 @@ import { EventData } from '../types/index.js';
 const prisma = new PrismaClient();
 
 export async function upsertEvent(event: EventData) {
-  const { id, status, startTime, sport, competition, scores, competitors } = event;
-  
-  await prisma.event.upsert({
-      where: { id },
-      update: {
-          status,
-          startTime: new Date(startTime),
-          sport,
-          competition,
-          removed: false,
-          scores: {
-              deleteMany: {},
-              create: {
-                  type: scores.CURRENT.type,
-                  home: scores.CURRENT.home,
-                  away: scores.CURRENT.away,
-              }
-          },
-          competitors: {
-              deleteMany: {},
-              create: [
-                  {
-                      type: competitors.HOME.type,
-                      name: competitors.HOME.name,
-                  },
-                  {
-                      type: competitors.AWAY.type,
-                      name: competitors.AWAY.name,
-                  }
-              ]
-          }
-      },
-      create: {
-          id,
-          status,
-          startTime: new Date(startTime),
-          sport,
-          competition,
-          removed: false,
-          scores: {
-              create: {
-                  type: scores.CURRENT.type,
-                  home: scores.CURRENT.home,
-                  away: scores.CURRENT.away,
-              }
-          },
-          competitors: {
-              create: [
-                  {
-                      type: competitors.HOME.type,
-                      name: competitors.HOME.name,
-                  },
-                  {
-                      type: competitors.AWAY.type,
-                      name: competitors.AWAY.name,
-                  }
-              ]
-          }
-      }
-  });
+    const { id, status, startTime, sport, competition, scores, competitors } = event;
+
+    const eventDto = {
+        id,
+        status,
+        startTime: new Date(startTime),
+        sport,
+        competition,
+        removed: status === 'REMOVED' ? true : false,
+        scores: {
+            create: {
+                type: scores.CURRENT.type,
+                home: scores.CURRENT.home,
+                away: scores.CURRENT.away,
+            },
+        },
+        competitors: {
+            create: [
+                {
+                    type: competitors.HOME.type,
+                    name: competitors.HOME.name,
+                },
+                {
+                    type: competitors.AWAY.type,
+                    name: competitors.AWAY.name,
+                },
+            ],
+        },
+    };
+
+    await prisma.event.upsert({
+        where: { id },
+        update: eventDto,
+        create: eventDto,
+    });
 }
 
 export async function markRemovedEvents(existingIds: string[]) {
-  await prisma.event.updateMany({
-    where: {
-      id: { notIn: existingIds }
-    },
-    data: { removed: true }
-  });
+    await prisma.event.updateMany({
+        where: {
+            id: { notIn: existingIds }
+        },
+        data: { status: 'REMOVED' }
+    });
 }
 
 export async function getAllEvents(): Promise<Prisma.EventGetPayload<{ include: { scores: true, competitors: true } }>[]> {
-  return await prisma.event.findMany({
-    where: { removed: false },
-    include: {
-      scores: true,
-      competitors: true
-    }
-  });
+    return await prisma.event.findMany({
+        where: { status: { not: 'REMOVED' } },
+        include: {
+            scores: true,
+            competitors: true
+        }
+    });
 }
